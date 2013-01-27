2d7b37efc354a199e75a302e60d995b67d19464e
TAO LIN <t4ol1n@gmail.com>
2013-01-27 23:45:01 +0800

    add View/Edit for User, add Admin Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,24 +2,72 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @nav_active = 'index'
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @nav_active = 'show'
   end
   
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    @user = User.new
+    @nav_active = 'new'
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(params[:user])
+    @nav_active = 'new'
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'user was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+    @nav_active = 'edit'
+  end
+
+
   def update
     authorize! :update, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
+    @nav_active = 'update'
     if @user.update_attributes(params[:user], :as => :admin)
       redirect_to users_path, :notice => "User updated."
     else
       redirect_to users_path, :alert => "Unable to update user."
     end
   end
-    
+
+  # GET /users/1
+  # GET /users/1.json
+  def delete
+    @user = User.find(params[:id])
+    @nav_active = 'delete'
+    respond_to do |format|
+      format.html # delete.html.erb
+      format.json { render json: @user }
+    end
+  end
+
   def destroy
     authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
     user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 AnyAgent::Application.routes.draw do
+  get "admin/index"
+
   resources :interviewees
 
   resources :interviewees do 
     member do
-	  get 'delete'
-	end
+	    get 'delete'
+	  end
   end
 
   authenticated :user do
-  		root :to => 'home#users'
+  	root :to => 'home#users'
   end
   root :to => "home#index"
   devise_for :users
-  resources :users
+  resources :users  do 
+    member do
+      get 'delete'
+    end
+  end
 
 
 end
